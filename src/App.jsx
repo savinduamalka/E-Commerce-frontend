5f5bd8bca3ad021c6aff0b18816ee9a637b9ca64
savinduamalka <savinduamalka@gmail.com>
2024-11-26 00:04:47 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter,  Route,  Routes,  } from "react-router-dom"
 import HomePage from "./pages/homePage"
+import Login from "./pages/login"
+import SignUp from "./pages/signup"
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
     </Routes>
     </BrowserRouter>
   
