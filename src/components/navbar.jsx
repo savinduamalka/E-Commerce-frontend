977de92fe170740e393d06cbbe05087fc80cd324
savinduamalka <savinduamalka@gmail.com>
2024-11-28 23:39:18 +0530

    add a cart icon with text to view cart

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { AiFillHome, AiOutlineCar } from "react-icons/ai";
-import { FiLogIn } from "react-icons/fi";
+import { FiLogIn, FiShoppingCart } from "react-icons/fi";
 import { MdContactPage } from "react-icons/md";
 
 function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white py-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
+    <nav className="py-4 text-white bg-blue-600 shadow-md">
+      <div className="container flex items-center justify-between mx-auto">
         <div className="flex items-center text-2xl font-bold">
           <img
             src="../logo.png"
             alt="Company Logo"
-            className="inline-block mr-2 h-12 w-12"
+            className="inline-block w-12 h-12 mr-2"
           />
           AutoMobile SL
         </div>
@@ -33,6 +33,12 @@ function Navbar() {
             <MdContactPage size={24} />
             <a href="/contact" className="hover:underline">
               Contact Us
+            </a>
+          </li>
+          <li className="flex items-center space-x-2">
+            <FiShoppingCart size={24} />
+            <a href="/cart" className="hover:underline">
+              Cart
             </a>
           </li>
           <li className="flex items-center space-x-2">
