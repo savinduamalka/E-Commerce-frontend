56f3ebd607b7e64b9923e28daabfc0324d0ccf00
savinduamalka <savinduamalka@gmail.com>
2024-12-04 23:12:07 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/login"
 import SignUp from "./pages/signup"
 import Contact from "./pages/contact"
 import Cart from "./pages/cart"
+import Product from "./pages/products"
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Route path="/signup" element={<SignUp/>}/>
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/cart" element={<Cart/>}/>
+      <Route path="/products" element={<Product />} />
     </Routes>
     </BrowserRouter>
   
