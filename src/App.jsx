a811522616b5026196d1a7a1c5586e7da0495a89
savinduamalka <savinduamalka@gmail.com>
2024-12-05 22:54:31 +0530

    create admin dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import { BrowserRouter,  Route,  Routes,  } from "react-router-dom"
-import HomePage from "./pages/homePage"
-import Login from "./pages/login"
-import SignUp from "./pages/signup"
-import Contact from "./pages/contact"
-import Cart from "./pages/cart"
-import Product from "./pages/products"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import Login from "./pages/login";
+import SignUp from "./pages/signup";
+import Contact from "./pages/contact";
+import Cart from "./pages/cart";
+import Product from "./pages/products";
+import AdminHome from "./pages/adminpages/adminHome";
 
 
 function App() {
-
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomePage/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/signup" element={<SignUp/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/cart" element={<Cart/>}/>
-      <Route path="/products" element={<Product />} />
-    </Routes>
-    </BrowserRouter>
-  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products" element={<Product />} />
+          <Route path="/admin" element={<AdminHome />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
