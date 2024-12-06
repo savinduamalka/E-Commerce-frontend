c757c023259fce83fe0d0a7b8e344a2f474998b9
savinduamalka <savinduamalka@gmail.com>
2024-12-06 14:46:23 +0530

    get and add saved token into the header

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_LARAVEL_API_URL || 'http://localhost:8000/api',
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-    },
+  baseURL: import.meta.env.VITE_LARAVEL_API_URL || "http://localhost:8000/api",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("auth_token"); // Get saved token from localStorage
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`; // Add the Bearer token to headers
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export { api };
