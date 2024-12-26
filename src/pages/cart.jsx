b6488b0b2cc1b3deda57be9209386ea3b4bd6b8c
savinduamalka <savinduamalka@gmail.com>
2024-12-27 02:11:16 +0530

    debug error

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/navbar";
 import { api } from "../lib/api";
-import { ToastContainer, toast } from "react-toastify";
-
 
 function Cart() {
   const [cartItems, setCartItems] = useState([]);
